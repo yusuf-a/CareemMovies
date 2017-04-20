83daf5a213bb6a935950917e1f11b8c343db94da
Yusuf Afghan <yusufa1991@gmail.com>
2017-04-20 17:46:16 +0100

    Initial commit

---
+++ CareemMovies/CareemMovies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CareemMovies
+//
+//  Created by Yusuf Afghan on 20/04/2017.
+//  Copyright © 2017 Yusuf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
