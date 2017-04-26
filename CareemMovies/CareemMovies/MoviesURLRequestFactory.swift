e36e4ccce636ce55e4cf178ca8ba0ecaa22d0009
Yusuf Afghan <yusufa1991@gmail.com>
2017-04-26 17:31:43 +0100

    Adds urlRequestFactory + tests

---
+++ CareemMovies/CareemMovies/MoviesURLRequestFactory.swift
@@ -0,0 +1,76 @@
+//
+//  MoviesURLRequestFactory.swift
+//  CareemMovies
+//
+//  Created by Yusuf Afghan on 26/04/2017.
+//  Copyright © 2017 Yusuf. All rights reserved.
+//
+
+import Foundation
+
+protocol URLRequestFactory {
+
+	
+}
+
+enum FindingDataType: String {
+	
+	case search = "search"
+	case discover = "discover"
+	case find = "find"
+}
+
+enum ContentType: String {
+	
+	case movies = "movies"
+	case tv = "tv"
+}
+
+enum Strings {
+	
+	static let apiKeyKey = "api_key"
+	static let apiKeyValue = "2696829a81b1b5827d515ff121700838"
+	static let baseURL = "http://api.themoviedb.org/3"
+}
+
+class DefaultURLRequestFactory: URLRequestFactory {
+	
+	func makeURLRequest(withPath path: String, httpParams: [[String: String]]) -> URLRequest? {
+		
+		let urlString = "\(Strings.baseURL)/\(path)"
+		
+		guard let url = URL(string: urlString) else {
+			
+			return nil
+		}
+		
+		var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: true)
+		
+		let queryItems = generateQueryItems(fromDictionary: httpParams)
+		
+		urlComponents?.queryItems = queryItems
+		
+		guard let urlWithHttpParams = urlComponents?.url else {
+			
+			return nil
+		}
+		
+		return URLRequest(url: urlWithHttpParams)
+	}
+	
+	private func generateQueryItems(fromDictionary dictionary: [[String: String]]) -> [URLQueryItem] {
+		
+		var queryItems = [URLQueryItem]()
+		
+		for param in dictionary {
+			
+			for (name, value) in param {
+				
+				let  queryItem = URLQueryItem(name: name, value: value)
+				
+				queryItems.append(queryItem)
+			}
+		}
+		return queryItems
+	}
+}
---
+++ CareemMovies/CareemMoviesTests/MoviesURLRequestFactory.swift
@@ -0,0 +1,43 @@
+//
+//  MoviesURLRequestFactory.swift
+//  CareemMovies
+//
+//  Created by Yusuf Afghan on 26/04/2017.
+//  Copyright © 2017 Yusuf. All rights reserved.
+//
+
+import XCTest
+
+@testable import CareemMovies
+
+class MoviesURLRequestFactory: XCTestCase {
+	
+	var urlRequestFactory: DefaultURLRequestFactory!
+    
+    override func setUp() {
+        super.setUp()
+		
+		urlRequestFactory = DefaultURLRequestFactory()
+    }
+    
+    override func tearDown() {
+		
+		urlRequestFactory = nil
+        super.tearDown()
+    }
+	
+	func testExpectedURL_IsReturned_WithParamaters() {
+		
+		let expectedString = "http://api.themoviedb.org/3/search/movie?api_key=2696829a81b1b5827d515ff121700838&query=batman"
+		
+		let expectedURL = URL(string: expectedString)
+		
+		let urlRequest = URLRequest(url: expectedURL!)
+		
+		let httpParams = [[Strings.apiKeyKey: Strings.apiKeyValue], ["query": "batman"]]
+		
+		let urlRequestFactoryURL = urlRequestFactory.makeURLRequest(withPath: "search/movie", httpParams: httpParams)
+		
+		XCTAssertEqual(urlRequest, urlRequestFactoryURL)
+	}
+}
