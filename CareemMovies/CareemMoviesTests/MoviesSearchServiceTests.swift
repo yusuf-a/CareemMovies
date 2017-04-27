c8f7e84f1388b6bb9d9e310349529b856e1ee0e6
Yusuf Afghan <yusufa1991@gmail.com>
2017-04-27 23:59:01 +0100

    Adds MoviesSearchService + tests

--- CareemMovies/CareemMoviesTests/MoviesSearchServiceTests.swift
+++ CareemMovies/CareemMoviesTests/MoviesSearchServiceTests.swift
@@ -8,28 +8,58 @@
 
 import XCTest
 
+@testable import CareemMovies
+
 class MoviesSearchServiceTests: XCTestCase {
+	
+	var moviesSearchService: MoviesSearchService!
+	var mockHTTPService: MockHTTPService!
+	var mockMovieSearchHTTPRequestFactory: MockMovieSearchHTTPRequestFactory!
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+		
+		mockHTTPService = MockHTTPService(urlSession: MockUrlSession())
+		mockMovieSearchHTTPRequestFactory = MockMovieSearchHTTPRequestFactory(urlRequestFactory: MockURLRequestFactory())
+		
+		moviesSearchService = MoviesSearchService(moviesSearchHTTPRequestFactory: mockMovieSearchHTTPRequestFactory, httpService: mockHTTPService)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+	
+	override func tearDown() {
+		
+		moviesSearchService = nil
+		mockHTTPService = nil
+		mockMovieSearchHTTPRequestFactory = nil
+		super.tearDown()
+	}
+	
+	func test_searchMovie_httpRequestFactory_searchWasCalled() {
+		
+		try! moviesSearchService.search(forMovie: "test", successCallback: { (_) in
+			
+		}, errorCallback: { (_) in
+
+		})
+		
+		XCTAssert(mockMovieSearchHTTPRequestFactory.searchWasCalled)
+	}
+	
+	func test_searchMovie_callsSuccessCallback_withMovies() {
+		
+		let testMovie = "batman"
+		
+		mockHTTPService.state = .success
+		
+		try! moviesSearchService.search(forMovie: testMovie, successCallback: { (movieViewModels) in
+			
+			XCTAssertNotNil(movieViewModels)
+			XCTAssertEqual(movieViewModels.count, 2)
+			XCTAssertEqual(movieViewModels[0].title, "Batman")
+			XCTAssertEqual(movieViewModels[0].imageUrl, "http://image.tmdb.org/t/p/w185//kBf3g9crrADGMc2AMAMlLBgSm2h.jpg")
+			XCTAssertEqual(movieViewModels[0].releaseDate, "June 23, 1989")
+			
+		}) { (_) in
+			XCTFail()
+		}
+	}
 }
