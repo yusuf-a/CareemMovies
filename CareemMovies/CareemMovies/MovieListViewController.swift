456aacc804a331d36284ecfcdf7513e7c8a7d89c
Yusuf Afghan <yusufa1991@gmail.com>
2017-04-29 11:24:38 +0100

    Adds searchResultsUpdater delegate method + show alert if no results found

--- CareemMovies/CareemMovies/MovieListViewController.swift
+++ CareemMovies/CareemMovies/MovieListViewController.swift
@@ -21,7 +21,7 @@ class MovieListViewController: UITableViewController {
 		
 		setUpSearchController()
 		setUpTableView()
-		getMovies_Test()
+		getMovies_Test(movie: "batman")
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -49,7 +49,9 @@ class MovieListViewController: UITableViewController {
 		searchController = UISearchController(searchResultsController: RecentSearchesTableViewController())
 		searchController?.searchBar.sizeToFit()
 		tableView.tableHeaderView = searchController?.searchBar
+		searchController?.searchBar.delegate = self
 		searchController?.delegate = self
+		searchController?.searchResultsUpdater = self
 		definesPresentationContext = true
 	}
 	
@@ -59,19 +61,40 @@ class MovieListViewController: UITableViewController {
 		tableView.estimatedRowHeight = 100
 	}
 	
-	private func getMovies_Test() {
+	internal func getMovies_Test(movie: String) {
 		
 		movieSearchService = MoviesSearchService()
-		try! movieSearchService?.search(forMovie: "batman", successCallback: { (viewmodels) in
+		try! movieSearchService?.search(forMovie: movie, successCallback: { (viewmodels) in
 			
 			self.movieViewModels = viewmodels
+			
 			DispatchQueue.main.async {
-				self.tableView.reloadData()
+				
+				self.updateViews()
 			}
 		}, errorCallback: { (error) in
 			
-			print(error.localizedDescription)
+			self.showAlertView(withTitle: "Uh oh! An error occurred", message: "Please try again later")
 		})
+	}
+	
+	private func updateViews() {
+		
+		tableView.reloadData()
+		
+		if movieViewModels.isEmpty {
+			
+			showAlertView(withTitle: "Uh oh! No results", message: "Please search again with a different movie")
+		}
+	}
+	
+	private func showAlertView(withTitle title: String, message: String) {
+		
+		let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+		let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+		alertController.addAction(action)
+		
+		present(alertController, animated: true, completion: nil)
 	}
 }
 
@@ -80,5 +103,34 @@ extension MovieListViewController: UISearchControllerDelegate {
 	func didPresentSearchController(_ searchController: UISearchController) {
 		
 		searchController.searchResultsController?.view.isHidden = false
+	}
+}
+
+extension MovieListViewController: UISearchBarDelegate {
+	
+	func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+		
+		searchController?.searchResultsController?.dismiss(animated: true, completion: nil)
+		
+		if let searchTerm = searchBar.text {
+
+			getMovies_Test(movie: searchTerm)
+		}
+	}
+}
+
+extension MovieListViewController: UISearchResultsUpdating {
+	
+	func updateSearchResults(for searchController: UISearchController) {
+		
+		guard let searchBarText = searchController.searchBar.text else {
+			
+			return
+		}
+		
+		if searchBarText.isEmpty {
+			
+			searchController.searchResultsController?.view.isHidden = false
+		}
 	}
 }
